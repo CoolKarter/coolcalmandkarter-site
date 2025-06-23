5df068459d7154a0d1e27158e9cf1556e8439cb1
CoolKarter <shop@coolcalmandkarter.com>
2025-06-23 19:01:10 -0400

    Updated Code

--- server/server.js
+++ server/server.js
@@ -141,6 +141,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:3000',
   'https://coolcalmandkarter.netlify.app',
+  'https://coolcalmandkarter.com'
 ];
 
 app.use(cors({
